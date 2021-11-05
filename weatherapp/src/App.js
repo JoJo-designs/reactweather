8ea766cf8a11c88967839055f43c0ef40de16084
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-05 11:52:38 -0400

    making Api Calls

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,23 +1,13 @@
-import React, { useState} from 'react';
+import React from 'react';
+import SearchBar from './components/searchbar';
 
 
 
 export default function App() {
-    const [weatherData, setWeatherData] = useState('')
-
-    const getWeather = () => {
-        if (weatherData === '') {
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=43.7001&lon=-79.4163&exclude=minutely,hourly,alerts&units=metric&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            });
-        }};
 
     return (
         <div>
-            {getWeather()}
-            <p>this is an app</p>
+            <SearchBar />
         </div>
     )
 }
---
+++ weatherapp/src/components/searchbar.js
@@ -0,0 +1,49 @@
+import React, { useState} from 'react';
+
+export default function SearchBar() {
+    
+    const [cityName, setCityName] = useState('');
+    const [weatherData, setWeatherData] = useState('')
+    const [lon, setLon] = useState('')
+    const [lat, setLat] = useState('')
+
+    const handleChange = (event) => {
+        const { target } = event;
+        const inputValue = target.value;
+        const inputType = target.name;
+
+        if (inputType === "cityName") {
+            setCityName(inputValue)
+            console.log(cityName)
+        }
+    }
+
+    const getWeather = () => {
+            fetch(`https://api.openweathermap.org/geo/1.0/direct?q=toronto&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
+            .then((res) => res.json())
+            .then((data) => {
+                setLat(data[0].lat) 
+                setLon(data[0].lon)
+                console.log(lat, lon)
+            });
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+            })
+        };
+
+        return (
+            <div>
+                <input
+                placeholder="Search a City"
+                value={cityName}
+                name="cityName"
+                type="text"
+                onChange={handleChange}
+                >
+                </input>
+                <button onClick={getWeather} >Search</button>
+            </div>    
+        )
+}
