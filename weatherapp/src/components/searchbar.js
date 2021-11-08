dfff45f929a6ca0ef31465b4efdd5e909f35bfb2
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-08 08:33:22 -0500

    build working search function that pulls in data

--- weatherapp/src/components/searchbar.js
+++ weatherapp/src/components/searchbar.js
@@ -4,8 +4,8 @@ export default function SearchBar() {
     
     const [cityName, setCityName] = useState('');
     const [weatherData, setWeatherData] = useState('')
-    const [lon, setLon] = useState('')
-    const [lat, setLat] = useState('')
+
+    
 
     const handleChange = (event) => {
         const { target } = event;
@@ -18,24 +18,38 @@ export default function SearchBar() {
         }
     }
 
-    const getGeo = () => {
-            fetch(`https://api.openweathermap.org/geo/1.0/direct?q=toronto&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
-            .then((res) => res.json())
-            .then((data) => {
-                setLat(data[0].lat) 
-                setLon(data[0].lon)
-                console.log(lat, lon)
-            });
-            getWeather()
-        };
+     async function getGeo() {
+        try {
+           const res = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
+            const data = await res.json();
+            console.log(data)
+        getWeather(data)
+        } catch (error) {
+            console.log(error)
+        }
+     } 
 
-    const getWeather = () => {
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            });
-    };
+
+     async function getWeather(data) {
+         try {
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data[0].lon}&exclude=minutely,hourly,alerts&units=metric&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
+            const weather = await res.json();
+            console.log(weather)
+         } catch(error) {
+             console.log(error)
+         }
+         
+     }
+
+    // const getWeather = (data) => {
+    //     console.log(data)
+    //     console.log(data[0].lat)
+        // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data[0].lat}&lon=${data.lon[0]}&exclude=minutely,hourly,alerts&units=metric&appid=89e0b7e8dbbac9434ed75176dac7f8a3`)
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         console.log(data)
+        //     });
+    // };
 
         return (
             <div>
