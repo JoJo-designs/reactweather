7a6f4f4c709a1bd233de466ef3d9d9e6f59f2c58
JoJo-designs <jodiladouceur.design@gmail.com>
2021-11-15 11:37:28 -0500

    working on getting city name from database

--- weatherapp/src/components/databody.js
+++ weatherapp/src/components/databody.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useIndexedDB } from 'react-indexed-db';
 // import React from 'react';
 
 export default function DataBody(weatherData) {
 
     const [dataExsit, setDataExsit] = useState('')
-    const [city, setCity] = useState('')
+    const { getByIndex } = useIndexedDB('cities');
+    const [city, setCity] = useState()
     
     
     useEffect(() => {
@@ -12,14 +14,24 @@ export default function DataBody(weatherData) {
         if (weatherData.weatherData !== '') {
             console.log("we have data") 
             setDataExsit(true)
+                let getLat = String(weatherData.weatherData.lat)
+                console.log(getLat)
+                getByIndex('lat', getLat).then(cityData => {
+                setCity(cityData);
+                console.log(city)
+            })
         }
     }, [weatherData])
+
+
+    console.log(city)
 
     return (
         <div>
             { dataExsit ? 
                 <div> 
-                    <h2>{weatherData.weatherData.timezone.split("/").pop()}</h2>
+                    {/* <h2>{weatherData.weatherData.timezone.split("/").pop()}</h2> */}
+                    <h2>this is city name</h2>
                     <h3>temp: {weatherData.weatherData.current.temp}</h3>
                 </div> 
             :
