15fcf2c1c0a65d18b399eccad061003f80dbb171
JoJo-designs <jodiladouceur.design@gmail.com>
2022-01-04 12:43:21 -0500

    more css working with weather images

--- weatherapp/src/components/body.js
+++ weatherapp/src/components/body.js
@@ -42,31 +42,31 @@ export default function Body(cityData) {
         console.log(appData.current.uvi)
         if (appData.current.uvi < 3) {
             console.log("UV is Low")
-            return ( <h3 className="low">UV Index: {appData.current.uvi}</h3> )
+            return ( <h3 className=" uv low">UV Index: {appData.current.uvi}</h3> )
         }
         if (appData.current.uvi < 4 &&  appData.current.uvi > 7) {
             console.log("UV is Mid")
-            return ( <h3 className="mid">UV Index: {appData.current.uvi}</h3> )
+            return ( <h3 className=" uv mid">UV Index: {appData.current.uvi}</h3> )
         }
         if (appData.current.uvi > 8) {
             console.log("UV is High")
-            return ( <h3 className="high">UV Index: {appData.current.uvi}</h3> )
+            return ( <h3 className=" uv high">UV Index: {appData.current.uvi}</h3> )
         }
     }
 
     // selectImage use weather data and returns a matching image. the ones the api
     // provides are very small.
     const selectImage = () => { 
-        if (appData.current.weather[0].id === 501) {
+        if (appData.current.weather[0].id === 500 || 501) {
             return <img src={lightRain}></img>
         }
-        if (appData.current.weather[0].id === 601) {
+        if (appData.current.weather[0].id === 600 || 601) {
             return <img src={snow}></img>
         }
         if (appData.current.weather[0].id === 800) {
             return <img src={sunny}></img>
         }
-        if (appData.current.weather[0].id === 802) {
+        if (appData.current.weather[0].id ===  801 || 802) {
             return <img src={scatteredClouds}></img>
         }
         if (appData.current.weather[0].id === 803 || 804) {
@@ -82,14 +82,21 @@ export default function Body(cityData) {
             { appData ?
             <div className="forecast">
                 <h2 className="bodyHeader">{capitalize()} Current Forecast</h2>
-                <div className="columns eight">
-                <Moment format="ddd MMM, D"></Moment>
-                <h3>temp: {Math.floor(appData.current.temp)}</h3>
-                <h3>humidity: {Math.round(appData.current.humidity)}</h3>
-                <h3>wind speed: {Math.round(appData.current.wind_speed)}</h3>
+                {/* dataBox wraps all the data */}
+                <div className="dataBox"> 
+                {/* data only wraps the data displayed with word */}
+                <div className="data">
+                {/* <Moment format="ddd MMM, D"></Moment> */}
+                <h3>Temp: {Math.floor(appData.current.temp)}</h3>
+                <h3>Humidity: {Math.round(appData.current.humidity)}</h3>
+                <h3>Wind Speed: {Math.round(appData.current.wind_speed)}</h3>
                 <div>{uvIndex()}</div>
                 </div>
-                <div className="columns three">{selectImage()}</div>
+                {/* weatherImg is just the image */}
+                <div className="weatherImg">{selectImage()}
+                <p>{appData.current.weather[0].description}</p>
+                </div>
+                </div>
             </div> 
             :
             <h2>Waiting for data...</h2>
